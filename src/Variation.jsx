38cb5d7032af2a9814247c9794c0f93d947bdcda
Jon Zachary <jontzachary@gmail.com>
2018-11-26 22:01:17 -0500

    fix bug in var display text

--- src/Variation.jsx
+++ src/Variation.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 
 const Variation = (props) => {
-  let isHidden = false;
-  const isHiddenClass = (props.isVarHidden) ? 'hideEval' : 'showEval' ;
-  const hideShowButtonText = (props.isVarHidden) ? 'Show' : 'Hide';
-  const { varMoves, start } = props;
+  const { varMoves, start, isVarHidden } = props;
+  const isHiddenClass = (isVarHidden) ? 'hideEval' : 'showEval' ;
+  const hideShowButtonText = (isVarHidden) ? 'Show' : 'Hide';
   const formattedMoves = []
   function formatMoves(){
     varMoves.forEach((m, i) => {
-      let mvNum = ((i + start) % 2 === 0) ? `${Math.floor((i + start / 2))}.` : null;
-      if (i === 0 && start % 2 === 1) {
-        mvNum = `${Math.ceil(i + start/ 2)}. ...`;
+      let halfMoveNum = i + start;
+      // If halfMoveNum is even then declare moveNum var equal to halfMoveNum
+      // Plus one, otherwise moveNum is null.
+      let mvNum = ((halfMoveNum) % 2 === 0) ? `${(halfMoveNum / 2) + 1 }.` : null;
+
+      // If the first halfmoves is odd then moveNum is equal to previous halfMoveNum
+      // Plus the string '...'. This works but leaves out line break on first move
+      if (halfMoveNum % 2 === 1 && i === 0) {
+        mvNum = `${Math.ceil(i + start / 2)}. \u2026`;
       }
       formattedMoves.push(
         <span key={i}>
           {mvNum} {m}
-          {(!mvNum) || (start % 2 === 1 && i === start) ? <br/> : ''}
+          {(!mvNum) || (halfMoveNum % 2 === 1 && i === 0)? <br/> : ''}
         </span>
       )
     })
-    return [formattedMoves];
+    console.log(formattedMoves);
+    return formattedMoves;
   }
   return (
     <div className="Eval2">
